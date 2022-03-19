503937e6713c8c10ff7b8709299bb0ff2a616f18
eclipseop <mh@otterdev.com>
2022-03-19 10:39:46 -0500

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,10 @@
+import { Product } from "@prisma/client";
 import { NextPage } from "next";
 import useSWR from "swr";
 
-type Product = {
-  name: string,
-  price: string,
-  image: string,
-  url: string,
-  from: string
-}
-
 const fetcher = (url: string) => fetch(url).then(r => r.json());
 
-const Item = ({name, price, image, from, url}: Product) => {
-  console.log(from);
+const Item = ({product}: { product: Product }) => {
 
   const abbreviate = (title: string) => {
     if (title.length < 50) return title;
@@ -22,10 +14,10 @@ const Item = ({name, price, image, from, url}: Product) => {
 
   return (
     <div className="border rounded p-2 h-72 hover:ring ring-blue-100 flex flex-col">
-      <img className="h-40 w-40 transition-height ease-in-out duration-300 hover:absolute hover:h-96 hover:w-96" src={`https://${image}`} />
-      <h1>{abbreviate(name)}</h1>
-      <h2>{price}</h2>
-      <a href={url} className="bg-blue-500 hover:bg-blue-600 text-white w-full p-1 rounded mt-1 text-center">Buy on <span className="font-semibold">{from}</span></a>
+      <img className="h-40 w-40 transition-height ease-in-out duration-300 hover:absolute hover:h-96 hover:w-96" src={`https://${product.image}`} />
+      <h1>{abbreviate(product.name)}</h1>
+      <h2>{product.price}</h2>
+      <a href={product.url} className="bg-blue-500 hover:bg-blue-600 text-white w-full p-1 rounded mt-1 text-center">Buy on <span className="font-semibold">{product.from}</span></a>
     </div>
   );
 };
@@ -39,13 +31,11 @@ const Home: NextPage = () => {
 
   const products: Product[] = data;
 
-  console.log(products);
-
   return (
     <div className="flex min-h-screen w-2/3 justify-center mx-auto">
       <div className="grid grid-cols-3 w-full gap-4 pt-4 h-full">
         {
-          products.filter((p) => parseInt(p.price.substring(1)) > 0).map((p) => <Item key={p.name} from={p.from} url={p.url} name={p.name} price={p.price} image={p.image} />)
+          products.filter((p) => parseInt(p.price.substring(1)) > 0).map((p) => <Item key={p.name} product={p} />)
         }
       </div>
     </div>
