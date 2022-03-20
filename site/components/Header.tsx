fe37462ea2d4cc4e30bd39d1bb0fe877b8ba5fb3
eclipseop <mh@otterdev.com>
2022-03-20 11:03:08 -0500

    favicon

--- site/components/Header.tsx
+++ site/components/Header.tsx
@@ -4,7 +4,6 @@ const Header = () => {
       <h1 className="font-semibold text-xl tracking-widest text-center">Keeb Finder</h1>
       <div className="flex flex-row w-full justify-around">
         <a href='keycaps'>Keycaps</a>
-        <a href='cases'>Case</a>
         <a href='switches'>Switches</a>
       </div>
     </header>
