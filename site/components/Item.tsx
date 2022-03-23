88a41a93f3bc720abad5115e114aa5b6489f14e6
eclipseop <mh@otterdev.com>
2022-03-22 19:09:05 -0500

    css changes

--- site/components/Item.tsx
+++ site/components/Item.tsx
@@ -3,7 +3,7 @@ import { Product } from "@prisma/client";
 const Item = ({product}: { product: Product }) => {
 
   const abbreviate = (name: string) => {
-    if (name.length < 50) return name;
+    if (name.length < 40) return name;
   
     return name.substring(0, 40) + "...";
   };
@@ -11,12 +11,12 @@ const Item = ({product}: { product: Product }) => {
   return (
     <div className="border rounded p-2 h-72 hover:ring ring-blue-200 flex flex-col bg-gray-100 border">
       <img 
-        className="h-40 w-40 rounded hover:h-96 hover:w-96 hover:absolute transition-height ease-in-out duration-100" 
+        className="h-40 w-40 rounded hover:h-96 hover:w-96 hover:absolute transition-height ease-in-out duration-75" 
         alt="keyboard image"
         src={`${product.image.startsWith('http') ? '' : 'https://'}${product.image}`} 
       />
       <div className="flex-grow">
-        <h1>{abbreviate(product.name)}</h1>
+        <h1 title={product.name}>{abbreviate(product.name)}</h1>
         <h2>From: {product.price}</h2>
       </div>
       <a href={product.url} target="_blank" className="bg-blue-500 hover:bg-blue-600 text-white w-full p-1 rounded text-center" rel="noreferrer">Buy on <span className="font-semibold">{product.from}</span></a>
