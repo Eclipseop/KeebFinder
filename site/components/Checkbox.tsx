fc23f8e428cddf0f9dea8b18e05bb4319f857fb3
eclipseop <mh@otterdev.com>
2022-03-26 20:22:43 -0500

    better onclick for checkbox

--- site/components/Checkbox.tsx
+++ site/components/Checkbox.tsx
@@ -7,8 +7,8 @@ type Props = {
 
 const Checkbox = ({array, value, click}: Props) => {
   return (
-    <div className="space-x-1">
-      <input className="space-x-3" type="checkbox" checked={array.includes(value)} onChange={click} />
+    <div className="space-x-1" onClick={click}>
+      <input className="space-x-3" type="checkbox" checked={array.includes(value)} />
       <label>{value}</label>
     </div>
   );
